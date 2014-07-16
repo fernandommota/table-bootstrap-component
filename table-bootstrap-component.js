92b818ce3a11357af21e21d3ec78492513323fd4
Fernando Maia da Mota <fernando.maia@it4biz.com.br>
2014-07-16 13:07:23 -0300

    Make changes for compatibility with datatables version 1.10.
    Updated the plugin TableTools version 2.2.2
    
    Resolved issue #3

--- table-bootstrap-component.js
+++ table-bootstrap-component.js
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2002 - 2013 Webdetails, a Pentaho company.  All rights reserved.
+* Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
 * 
 * This software was developed by Webdetails and is provided under the terms
 * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -51,7 +51,118 @@ if($.fn.dataTableExt != undefined){
  *    oTable.fnLengthChange( 100 );
  * } );
  */
+
+  /* If table elements have class 'numeric' and values are represented as strings (e.g. "234456.5675")
+   let's try and convert them to numeric values for sorting. Source based on version 1.7.5 of dataTables.js */
+  $.extend( $.fn.dataTableExt.oSort, {
+    "numeric-asc": function ( a, b ) {
+      a = (a=="-"||a=="") ? 0 : ($.isNumeric(a) ? a*1 : 0);
+      b = (b=="-"||b=="") ? 0 : ($.isNumeric(b) ? b*1 : 0);
+      return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "numeric-desc": function ( a, b ) {
+      a = (a=="-"||a=="") ? 0 : ($.isNumeric(a) ? a*1 :0);
+      b = (b=="-"||b=="") ? 0 : ($.isNumeric(b) ? b*1 :0);
+      return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+  });
 }
+
+/*
+ * DataTables 1.10 `two_button` pagination control compatibility file
+ */
+if(jQuery.fn.DataTable != undefined && jQuery.fn.DataTable.ext != undefined) {
+  jQuery.extend(jQuery.fn.DataTable.ext.classes, {
+    "sNoFooter" : "",
+    "sPagePrevEnabled": "paginate_enabled_previous",
+    "sPagePrevDisabled": "paginate_disabled_previous",
+    "sPageNextEnabled": "paginate_enabled_next",
+    "sPageNextDisabled": "paginate_disabled_next"
+  });
+
+  jQuery.extend(jQuery.fn.DataTable.ext.oJUIClasses, {
+    "sNoFooter" : "",
+    "sSortable" : "",
+    "sSortAsc" : "",
+    "sSortDesc" : "",
+    "sSortColumn" : "",
+    "sPagePrevEnabled": "fg-button ui-button ui-state-default ui-corner-left",
+    "sPagePrevDisabled": "fg-button ui-button ui-state-default ui-corner-left ui-state-disabled",
+    "sPageNextEnabled": "fg-button ui-button ui-state-default ui-corner-right",
+    "sPageNextDisabled": "fg-button ui-button ui-state-default ui-corner-right ui-state-disabled",
+    "sPageJUINext": "ui-icon ui-icon-circle-arrow-e",
+    "sPageJUIPrev": "ui-icon ui-icon-circle-arrow-w"
+  });
+
+  jQuery.extend(jQuery.fn.DataTable.ext.pager, {
+    "two_button": {
+      "fnInit": function (oSettings, nPaging, fnCallbackDraw) {
+        var oLang = oSettings.oLanguage.oPaginate;
+        var oClasses = oSettings.oClasses;
+        var fnClickHandler = function (e) {
+          if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
+            fnCallbackDraw(oSettings);
+          }
+        };
+
+        var sAppend = (!oSettings.bJUI) ?
+          '<a class="' + oSettings.oClasses.sPagePrevDisabled + '" tabindex="' + oSettings.iTabIndex + '" role="button">' + oLang.sPrevious + '</a>' +
+          '<a class="' + oSettings.oClasses.sPageNextDisabled + '" tabindex="' + oSettings.iTabIndex + '" role="button">' + oLang.sNext + '</a>' :
+          '<a class="' + oSettings.oClasses.sPagePrevDisabled + '" tabindex="' + oSettings.iTabIndex + '" role="button"><span class="' + oSettings.oClasses.sPageJUIPrev + '"></span></a>' +
+          '<a class="' + oSettings.oClasses.sPageNextDisabled + '" tabindex="' + oSettings.iTabIndex + '" role="button"><span class="' + oSettings.oClasses.sPageJUINext + '"></span></a>';
+        $(nPaging).append(sAppend);
+
+        var els = $('a', nPaging);
+        var nPrevious = els[0],
+          nNext = els[1];
+
+        oSettings.oApi._fnBindAction(nPrevious, {
+          action: "previous"
+        }, fnClickHandler);
+        oSettings.oApi._fnBindAction(nNext, {
+          action: "next"
+        }, fnClickHandler);
+
+        /* ID the first elements only */
+        if (!oSettings.aanFeatures.p) {
+          nPaging.id = oSettings.sTableId + '_paginate';
+          nPrevious.id = oSettings.sTableId + '_previous';
+          nNext.id = oSettings.sTableId + '_next';
+
+          nPrevious.setAttribute('aria-controls', oSettings.sTableId);
+          nNext.setAttribute('aria-controls', oSettings.sTableId);
+        }
+      },
+
+      "fnUpdate": function (oSettings, fnCallbackDraw) {
+        if (!oSettings.aanFeatures.p) {
+          return;
+        }
+
+        var oClasses = oSettings.oClasses;
+        var an = oSettings.aanFeatures.p;
+        var nNode;
+
+        /* Loop over each instance of the pager */
+        for (var i = 0, iLen = an.length; i < iLen; i++) {
+          nNode = an[i].firstChild;
+          if (nNode) {
+            /* Previous page */
+            nNode.className = (oSettings._iDisplayStart === 0) ?
+            oClasses.sPagePrevDisabled : oClasses.sPagePrevEnabled;
+
+            /* Next page */
+            nNode = nNode.nextSibling;
+            nNode.className = (oSettings.fnDisplayEnd() == oSettings.fnRecordsDisplay()) ?
+            oClasses.sPageNextDisabled : oClasses.sPageNextEnabled;
+          }
+        }
+      }
+    }
+  });
+}
+
 
 var TableBootstrap = UnmanagedComponent.extend({
   
@@ -63,7 +174,7 @@ var TableBootstrap = UnmanagedComponent.extend({
       return;
     }
     if(!this.htmlObject) {
-      return this.error("TableComponent requires an htmlObject");
+      return this.error("TableBootstrap requires an htmlObject");
     }
     try{
       this.block();
@@ -76,7 +187,7 @@ var TableBootstrap = UnmanagedComponent.extend({
          */
         var success = _.bind(function(data){
           this.rawData = data;
-          this.processTableComponentResponse(data)
+          this.processTableBootstrapResponse(data)
         },this);
         var handler = this.getSuccessHandler(success);
 
@@ -88,7 +199,7 @@ var TableBootstrap = UnmanagedComponent.extend({
        * Something went wrong and we won't have handlers firing in the future
        * that will trigger unblock, meaning we need to trigger unblock manually.
        */
-      this.dashboard.error(e);
+      Dashboards.log(e,'exception');
       this.unblock();
     }
   },
@@ -108,7 +219,7 @@ var TableBootstrap = UnmanagedComponent.extend({
       if (changedValues != undefined) {
         values = changedValues;
       }
-      this.processTableComponentResponse(values);
+      this.processTableBootstrapResponse(values);
     },this);
 
     this.queryState.setCallback(success);
@@ -154,20 +265,20 @@ var TableBootstrap = UnmanagedComponent.extend({
       }
       return null;
     }
-    var sortingCols = p("iSortingCols"),sort = [];
-    if (sortingCols > 0) {
-      for (var i = 0; i < sortingCols; i++) {
-        var col = p("iSortCol_" + i);
-        var dir = p("sSortDir_" + i);
-        sort.push( col + (dir == "asc" ? "A" : "D"));
+    var sortingCols = p("order"),sort = [];
+    if(sortingCols.length > 0) {
+      for(var i = 0; i < sortingCols.length; i++ ) {
+      var col = sortingCols[i].column;
+      var dir = sortingCols[i].dir;
+      sort.push(col + (dir == "asc" ? "A" : "D"));
       }
     }
     var query = this.queryState,
     myself = this;
     query.setSortBy(sort.join(","));
-    query.setPageSize(parseInt(p("iDisplayLength")));
-    query.setPageStartingAt(p("iDisplayStart"));
-    query.setSearchPattern(p("sSearch") ? p("sSearch") : "");
+    query.setPageSize(parseInt(p("length")));
+    query.setPageStartingAt(p("start"));
+    query.setSearchPattern(p("search") ? p("search") : "");
     query.fetchData(function(d) {
       if (myself.postFetch){
         var mod = myself.postFetch(d,dataTable);
@@ -213,13 +324,15 @@ var TableBootstrap = UnmanagedComponent.extend({
            * where we didn't find a matching addIn
            */
           if(!foundAddIn && cd.colFormats) {
-            var position = dataTable.fnGetPosition(td),
-                rowIdx = position[0],
+            var position = dataTable.fnGetPosition(td);
+            if(position && typeof position[0] == "number"){
+              var rowIdx = position[0],
                 colIdx = position[2],
                 format = cd.colFormats[colIdx],
                 value = myself.rawData.resultset[rowIdx][colIdx];
-            if (format && (typeof value != "undefined" && value !== null)) {
-              $(td).text(sprintf(format,value));
+              if (format && (typeof value != "undefined" && value !== null)) {
+                $(td).text(sprintf(format,value));
+              }
             }
           }
       });
@@ -258,7 +371,10 @@ var TableBootstrap = UnmanagedComponent.extend({
    */
   handleAddIns: function(dataTable, td) {
     var cd = this.chartDefinition,
-        position = dataTable.fnGetPosition(td),
+        position = dataTable.fnGetPosition(td);
+        if(position && typeof position[0] != "number"){
+          return false;
+        }
         rowIdx = position[0],
         colIdx = position[2],
         colType = cd.colTypes[colIdx],
@@ -289,18 +405,17 @@ var TableBootstrap = UnmanagedComponent.extend({
       addIn.call(td,state,this.getAddInOptions("colType",addIn.getName()));
       return true;
     } catch (e) {
-      this.dashboard.error(e);
+      Dashboards.log(e,'exception');
       return false;
     }
   },
 
-  processTableComponentResponse : function(json) {
-
+  processTableBootstrapResponse : function(json) {
     var myself = this,
         cd = this.chartDefinition,
         extraOptions = {};
 
-    this.ph.trigger('cdfTableComponentProcessResponse');
+    this.ph.trigger('cdfTableBootstrapProcessResponse');
 
     // Set defaults for headers / types
     if(typeof cd.colHeaders === "undefined" || cd.colHeaders.length == 0)
@@ -315,11 +430,8 @@ var TableBootstrap = UnmanagedComponent.extend({
     $.each(this.extraOptions ? this.extraOptions : {}, function(i,e){
       extraOptions[e[0]] = e[1];
     });
-
-    //console.log(dtData0);
-    //console.log(extraOptions);
-
     var dtData = $.extend(cd.dataTableOptions,dtData0,extraOptions);
+
 
     /* Configure the table event handlers */
     dtData.fnDrawCallback = _.bind(this.fnDrawCallback,this);
@@ -338,21 +450,16 @@ var TableBootstrap = UnmanagedComponent.extend({
     if (json) {
       dtData.aaData = json.resultset;
     }
- 
-    this.ph.html("<table id='" + this.htmlObject + "Table' class='table table-striped table-bordered' width='100%'></table>");
+    
+    var tableClassName = dtData.tableStyle == "bootstrap" ?  'table table-striped table-bordered form-inline table-responsive' : 'tableComponent';
+  
+    this.ph.html("<table id='" + this.htmlObject + "Table' class='" + tableClassName +"' width='100%'></table>");
+
     /* 
      * We'll first initialize a blank table so that we have a
      * table handle to work with while the table is redrawing
      */
     this.dataTable = $("#"+this.htmlObject+'Table').dataTable(dtData);
-
-        //set the default class 
-    $('#'+this.htmlObject).addClass('form-inline table-responsive');
-    // modify table search input
-    $('div.dataTables_filter input').addClass("form-control input-sm center-block"); 
-    // modify table length select
-    $('div.dataTables_length select').addClass("form-control input-sm"); 
-
 
     // We'll create an Array to keep track of the open expandable rows.
     this.dataTable.anOpen = [];
@@ -391,7 +498,7 @@ var TableBootstrap = UnmanagedComponent.extend({
         }
       }
     });
-    myself.ph.trigger('cdfTableComponentFinishRendering');
+    myself.ph.trigger('cdfTableBootstrapFinishRendering');
   },
 
   handleExpandOnClick: function(event) {
@@ -454,7 +561,7 @@ var TableBootstrap = UnmanagedComponent.extend({
 {
   getDataTableOptions : function(options) {
     var dtData = {};
-
+	
     if(options.tableStyle == "themeroller"){
       dtData.bJQueryUI = true;
     }
@@ -464,43 +571,37 @@ var TableBootstrap = UnmanagedComponent.extend({
     dtData.bPaginate = options.paginate;
     dtData.bSort = options.sort;
     dtData.bFilter = options.filter;
-    dtData.sPaginationType = options.paginationType;
+    dtData.sPaginationType = options.paginationType;   
     dtData.aaSorting = options.sortBy;
-     
-    //sDom configurations, need for booststrap layout    
-    dtData.sDom = options.sDom == "undefined" ?
-    			 "C<'clear'>lfrtip<'row'<'col-xs-6'><'col-xs-6'>><'row'<'col-xs-6'><'col-xs-6'>>":
-			  options.sDom;
-    
+    dtData.tableStyle = options.tableStyle;
+	
+	//sDom configurations, need for booststrap layout    
+    if (typeof options.sDom == "string"){
+        dtData.sDom = options.sDom;//TODO: er...
+    }
+      else {
+        dtData.sDom = "CT<'clear'>lfrtip<'row'<'col-xs-6'><'col-xs-6'>><'row'<'col-xs-6'><'col-xs-6'>>";
+    }
+	
     if (typeof options.oLanguage == "string"){
       dtData.oLanguage = eval("(" + options.oLanguage + ")");//TODO: er...
-  	}
-      else {
-        dtData.oLanguage = options.oLanguage;
-  	}
-
-    if (typeof options.oColVis == "string"){
+    }
+    else {
+      dtData.oLanguage = options.oLanguage;
+    }
+	
+	if (typeof options.oColVis == "string"){
         dtData.oColVis = eval("(" + options.oColVis + ")");//TODO: er...
     }
       else {
         dtData.oColVis = options.oColVis;
     }
 
-    if (typeof options.oStdClasses == "string"){
-        dtData.oStdClasses = eval("(" + options.oStdClasses + ")");//TODO: er...
+    if (typeof options.language == "string"){
+      dtData.language = eval("(" + options.language + ")");//TODO: er...
+    } else {
+      dtData.language = options.language;
     }
-      else {
-        dtData.oStdClasses = options.oStdClasses;
-    }
-
-
-    if (typeof options.oTableTools == "string"){
-        dtData.oTableTools = eval("(" + options.oTableTools + ")");//TODO: er...
-    }
-      else {
-        dtData.oTableTools = options.oTableTools;
-    }
-
 
     if(options.colHeaders != undefined){
       dtData.aoColumns = new Array(options.colHeaders.length);
@@ -523,7 +624,7 @@ var TableBootstrap = UnmanagedComponent.extend({
         })
       };  // colTypes
       if(options.colFormats!=undefined){
-      // Changes are made directly to the json
+        // Changes are made directly to the json
 
       };  // colFormats
 
